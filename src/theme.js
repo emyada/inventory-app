44209cd9b5381d4fc8febfb3db9701a343ba861a
emyada <yadaclk1995@gmail.com>
2026-09-11 01:03:22 +0700

    Update theme New Version2.js

--- src/theme.js
+++ src/theme.js
@@ -35,5 +35,5 @@ export const todayStr = () => {
   const year = d.getFullYear();
   const month = String(d.getMonth() + 1).padStart(2, '0');
   const day = String(d.getDate()).padStart(2, '0');
-  return `${year}-${month}-${day}`;
+  return year + '-' + month + '-' + day;
 };
